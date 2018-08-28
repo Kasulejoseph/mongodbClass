aa888c3ea5a5848a1dac41daec00745d20531a74
kasulejoseph <kasule06joseph@gmail.com>
2018-08-28 10:41:36 +0300

    post a fruit

--- world.py
+++ world.py
@@ -6,6 +6,12 @@ def index():
     #return bottle.template('hello_world',username="kasule", things=mythings)
     return bottle.template('hello_world',{'username':'joseph','things':mythings})
 
+@bottle.post('/favorite')
+def favorite():
+    fruit = bottle.request.forms.get('fruit')
+    if (fruit ==None or fruit == ''):
+        return "No fruit"
+    return bottle.template('fruit',{'fruit':fruit})
 bottle.debug(True)
 bottle.run(host='localhost', port=8080)
     
